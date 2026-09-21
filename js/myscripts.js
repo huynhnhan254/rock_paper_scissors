36b21f13e4e8f4df0e5d7b2f12f51c08fe49647f
Ngoc <huynhnhan254@gmail.com>
2026-09-20 22:50:34 -0500

    Completed the game with 5 rounds with start button

--- js/myscripts.js
+++ js/myscripts.js
@@ -6,7 +6,6 @@ const log = document.querySelector("#log")
 
 function getComputerChoice() {
     let compChoice = Math.floor(Math.random() * max);
-
     
     return compChoice;
 }
@@ -15,23 +14,44 @@ function getHumanChoice() {
     let humanChoice = prompt("Enter your choice: ");   
     humanChoice = humanChoice.toLocaleLowerCase(); 
 
-    if (humanChoice === 'r') {
+    if (humanChoice === 'r' || humanChoice === "rock") {
         humanChoice = 0;
-    } else if (humanChoice === 'p') {
+    } else if (humanChoice === 'p' || humanChoice === "paper") {
         humanChoice = 1;
-    } else if (humanChoice === 's') {
+    } else if (humanChoice === 's' || humanChoice === "scissors") {
         humanChoice = 2;
-    }
+    } 
+
+    return humanChoice;
 }  
 
 
 function playRound(humanChoice, compChoice) {
-    if (humanChoice === compChoice) {
+    let res = humanChoice - compChoice;
+
+    if (res === 0) {
         console.log("Tie");
-    } else if (humanChoice === 1)
+    } else if (res === 1) {
+       humanScore++;
+    } else if (res === -1) {
+        computerScore++;
+    } else if (res === 2) {
+        computerScore++;
+    } else if (res === -2) {
+        humanScore++;
+    }
+
+    console.log("res: " + res);
+
+    console.log(humanScore, computerScore);
 }
 
-const humanSelection = getHumanChoice();
-const computerSelection = getComputerChoice();
+startButton.addEventListener("click", ()=>{
+    for (let i = 0; i < 5; i++) {
+        const humanSelection = getHumanChoice();
+        const computerSelection = getComputerChoice(i+1);
 
-playRound(humanSelection, computerSelection);
+        playRound(humanSelection, computerSelection);
+        console.log(i+1 + " times");
+    }
+});
